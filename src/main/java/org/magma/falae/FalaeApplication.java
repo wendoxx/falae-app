14e1b2ac0118fd64d19a827d37c95b5079f34fe5
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-09-18 21:23:13 -0300

    first commit

---
+++ src/main/java/org/magma/falae/FalaeApplication.java
@@ -0,0 +1,13 @@
+package org.magma.falae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalaeApplication.class, args);
+	}
+
+}
