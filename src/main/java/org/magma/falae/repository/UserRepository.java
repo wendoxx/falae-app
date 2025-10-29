580c3247f7013ad24c396b92f6d057ad713792e4
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-10-29 09:07:31 -0300

    feat: add user repository

--- src/main/java/org/magma/falae/repository/UserRepository.java
+++ src/main/java/org/magma/falae/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package org.magma.falae.repository;
 
-public interface UserRepository {
+import org.magma.falae.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByEmail(String email);
 }
