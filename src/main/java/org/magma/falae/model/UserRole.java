f7faef6fc6cf747912e8e838c8992ff16fc202cb
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-10-29 09:32:04 -0300

    feat: add user role

---
+++ src/main/java/org/magma/falae/model/UserRole.java
@@ -0,0 +1,16 @@
+package org.magma.falae.model;
+
+public enum UserRole {
+    USER("user"),
+    ADMIN("admin");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
