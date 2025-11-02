2532804bd7742ef3c784339d4b6b2decaca923ed
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-11-01 23:38:47 -0300

    feat: update user model

--- src/main/java/org/magma/falae/model/User.java
+++ src/main/java/org/magma/falae/model/User.java
@@ -36,12 +36,12 @@ public class User implements UserDetails {
 
     @Override
     public String getPassword() {
-        return "";
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return email;
     }
 
     @Override
