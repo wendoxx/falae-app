aff7abc29cea8271ccb306a2be09536c193263a2
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-11-04 10:18:04 -0300

    feat: add authentication service

--- src/main/java/org/magma/falae/dto/request/RegisterRequestDTO.java
+++ src/main/java/org/magma/falae/dto/request/RegisterRequestDTO.java
@@ -2,5 +2,7 @@ package org.magma.falae.dto.request;
 
 import org.magma.falae.model.UserRole;
 
-public record RegisterRequestDTO(String email, String username, String password, String birthdate, String phoneNumber, UserRole role) {
+import java.util.Date;
+
+public record RegisterRequestDTO(String email, String username, String password, Date birthdate, String phoneNumber, UserRole role) {
 }
