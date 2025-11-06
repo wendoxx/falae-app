88aba17eef1fceaad01a89f500ca3fff370e29ae
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-11-06 10:37:16 -0300

    feat: add many to many relationship to chat and user for participants attribute

--- src/main/java/org/magma/falae/model/Chat.java
+++ src/main/java/org/magma/falae/model/Chat.java
@@ -1,12 +1,12 @@
 package org.magma.falae.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.magma.falae.dto.response.ChatResponseDTO;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
@@ -18,8 +18,24 @@ public class Chat {
 
     @Id
     private UUID id;
+
     @OneToMany(mappedBy = "chat")
+
     private Set<Message> messages;
+
     private boolean isGroupChat;
-    //TODO: implement participants field and decide how collection type it will be
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "chat_participants", // Nome da tabela que liga Chats e Users
+            joinColumns = @JoinColumn(name = "chat_id"), // Chave estrangeira para ESTA classe (Chat)
+            inverseJoinColumns = @JoinColumn(name = "user_id") // Chave estrangeira para a OUTRA classe (User)
+    )
+    private Set<User> participants = new HashSet<>();
+
+    public Chat(ChatResponseDTO chatResponseDTO){
+        this.id = chatResponseDTO.id();
+        this.messages = chatResponseDTO.messages();
+        this.isGroupChat = chatResponseDTO.isGroupChat();
+    }
 }
--- src/main/java/org/magma/falae/model/User.java
+++ src/main/java/org/magma/falae/model/User.java
@@ -8,14 +8,12 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 
 @Entity
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "users")
 public class User implements UserDetails {
@@ -29,6 +27,8 @@ public class User implements UserDetails {
     private String phoneNumber;
     private Date birthDate;
     private UserRole role;
+    @ManyToMany(mappedBy = "participants", fetch = FetchType.LAZY)
+    private Set<Chat> chats = new HashSet<>();
 
     public User(String email, String username, String password, String phoneNumber, Date birthDate, UserRole role) {
         this.email = email;
