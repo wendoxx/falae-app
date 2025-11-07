42eacd99232d010838f7dd13ff3386055ca66329
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-11-07 10:06:14 -0300

    refactor: delete message attribute

--- src/main/java/org/magma/falae/dto/request/ChatRequestDTO.java
+++ src/main/java/org/magma/falae/dto/request/ChatRequestDTO.java
@@ -1,10 +1,7 @@
 package org.magma.falae.dto.request;
 
-import org.magma.falae.model.Message;
-import org.magma.falae.model.User;
-
 import java.util.Set;
 import java.util.UUID;
 
-public record ChatRequestDTO(UUID id, Set<Message> messages, boolean isGroupChat, Set<User> participants) {
+public record ChatRequestDTO(UUID id, boolean isGroupChat, Set<UUID> participants) {
 }
