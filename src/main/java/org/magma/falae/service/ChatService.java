c607280037b268b9909ab3c94659bcc055dbbdcf
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-11-07 10:13:06 -0300

    chore: comment create private chat class

--- src/main/java/org/magma/falae/service/ChatService.java
+++ src/main/java/org/magma/falae/service/ChatService.java
@@ -1,12 +1,37 @@
 package org.magma.falae.service;
 
+import org.magma.falae.dto.request.ChatRequestDTO;
+import org.magma.falae.dto.response.ChatResponseDTO;
+import org.magma.falae.model.Chat;
+import org.magma.falae.model.User;
+import org.magma.falae.repository.ChatRepository;
+import org.magma.falae.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class ChatService {
-    public void createChat() {
-        // Implementation goes here
-    }
+
+    @Autowired
+    private ChatRepository chatRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    /*public ChatResponseDTO createPrivateChat(ChatRequestDTO chatRequestDTO) {
+        Chat chat;
+
+        add variable for user 1 and user 2 to method parameters
+
+        if(chatRepository.findPrivateChatByParticipants()){
+            // Logic for creating one-on-one chat
+        } else {
+            // Logic for creating group chat
+        }
+        return null; // Replace with actual return
+    }*/
 
     public void getChat() {
         // Implementation goes here
