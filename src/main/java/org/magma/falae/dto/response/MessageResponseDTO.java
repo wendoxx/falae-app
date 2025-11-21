1078b2f8e64b7e1e88148261ca08075bac151fda
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-11-21 20:32:56 -0300

    feat: update message dto

--- src/main/java/org/magma/falae/dto/response/MessageResponseDTO.java
+++ src/main/java/org/magma/falae/dto/response/MessageResponseDTO.java
@@ -1,7 +1,8 @@
 package org.magma.falae.dto.response;
 
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record MessageResponseDTO(UUID userId, UUID chatId , String content, LocalDateTime timestamp) {
+public record MessageResponseDTO(UUID id, UUID userId, UUID chatId, String content, LocalDateTime timestamp) {
 }
