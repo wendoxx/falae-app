0556e45ede0b3b8da5b37c5d31bb30be13fc784e
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-11-22 17:19:41 -0300

    feat: update message controller

--- src/main/java/org/magma/falae/controller/MessageController.java
+++ src/main/java/org/magma/falae/controller/MessageController.java
@@ -1,4 +1,39 @@
 package org.magma.falae.controller;
 
+import org.magma.falae.dto.request.MessageRequestDTO;
+import org.magma.falae.dto.response.MessageResponseDTO;
+import org.magma.falae.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/messages")
 public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @PostMapping("/send-message")
+    public ResponseEntity<MessageResponseDTO> sendMessage(MessageRequestDTO messageRequestDTO) {
+        return ResponseEntity.status(201).body(messageService.createMessage(messageRequestDTO));
+    }
+
+    @PutMapping("/update-message")
+    public ResponseEntity<MessageResponseDTO> updateMessage(MessageRequestDTO messageRequestDTO) {
+        return ResponseEntity.status(201).body(messageService.updateMessage(messageRequestDTO));
+    }
+
+    @GetMapping("/message/{id}")
+    public ResponseEntity<MessageResponseDTO> getMessageById(@PathVariable UUID id) {
+        return ResponseEntity.ok(messageService.getMessageById(id));
+    }
+
+    @DeleteMapping("/delete-message/{id}")
+    public ResponseEntity<Void> deleteMessage(@PathVariable UUID id) {
+        messageService.deleteMessageById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
