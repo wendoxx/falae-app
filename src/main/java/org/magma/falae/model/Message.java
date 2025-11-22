44f1cf10f665434a6fb3bbab936c0fbff22cfca4
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2025-11-21 21:17:58 -0300

    feat: update message class

--- src/main/java/org/magma/falae/model/Message.java
+++ src/main/java/org/magma/falae/model/Message.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.magma.falae.dto.request.MessageRequestDTO;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -12,7 +13,6 @@ import java.util.UUID;
 
 @Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class Message implements Serializable {
     @Serial
@@ -22,10 +22,20 @@ public class Message implements Serializable {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String content;
-    private String sender;
+
+    @ManyToOne (fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id")
+    private User sender;
     private LocalDateTime timestamp;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "chat_id")
     private Chat chat;
+
+    public Message (MessageRequestDTO messageRequestDTO, User sender, Chat chat) {
+        this.content = messageRequestDTO.content();
+        this.sender = sender;
+        this.chat = chat;
+        this.timestamp = LocalDateTime.now();
+    }
 }
